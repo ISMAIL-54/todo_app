2d2f4e3ad2b235a0a358258bda5d26d222413a9e
ISMAIL-54 <ismail.00.me@gmail.com>
2025-11-25 13:49:17 +0100

    Add POST form to add new tasks

--- todo/views.py
+++ todo/views.py
@@ -1,9 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Task
 
 # Create your views here.
 def task_list(request):
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        if title:
+            Task.objects.create(title=title)
+        return redirect('task_list')
+
     tasks = Task.objects.all()
     return render(request, 'tasks/task_list.html', {'tasks': tasks}) 
 
