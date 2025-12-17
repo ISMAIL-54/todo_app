ee698502beee21367c5bd1eb11b036e6151750c0
ISMAIL-54 <imouhane@percallgroup.com>
2025-12-17 14:57:36 +0100

    Update views, and front-end

--- todo/models.py
+++ todo/models.py
@@ -4,7 +4,6 @@ class Task(models.Model):
     title = models.CharField(max_length=200)
     completed = models.BooleanField(default=False)
     description = models.TextField(blank=True, null=True)
-    deadline = models.DateTimeField(auto_now_add=True, blank=True)
-
+    deadline = models.DateField(blank=True, null=True)
     def __str__(self):
         return self.title
--- todo/views.py
+++ todo/views.py
@@ -1,22 +1,29 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
 from .models import Task
 
-# Adjust bootstrap
-#def gui(request):
-#   return render(request, 'tasks/task_list.html')
-
 def task_list(request):
-    if request.method == 'POST':
+    tasks = Task.objects.all()
+    return render(request, 'tasks/task_list.html', {'tasks': tasks}) 
+
+def add_task(request):
+    if request.method == "POST":
         title = request.POST.get('title')
         description = request.POST.get('description')
         deadline = request.POST.get('deadline')
         if title:
             Task.objects.create(title=title, description=description, deadline=deadline if deadline else None)
-        return redirect('task_list')
+    return redirect('task_list')
 
-    tasks = Task.objects.all()
-    return render(request, 'tasks/task_list.html', {'tasks': tasks}) 
+def edit_task(request, task_id):
+    task = get_object_or_404(Task, pk=task_id)
+    if request.method == "POST":
+        task.title = request.POST.get("title", "").strip()
+        task.description = request.POST.get("description", "").strip()
+        if request.POST.get("deadline", "").strip():
+            task.deadline = request.POST.get("deadline")
+        task.save()
+    return redirect("task_list") 
 
 def delete_task(request, task_id):
     task = Task.objects.get(id=task_id)
