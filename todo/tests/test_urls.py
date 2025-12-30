a18baa506665ba06c29da4b062d4d9d46e1af040
ISMAIL-54 <imouhane@percallgroup.com>
2025-12-30 17:54:35 +0100

    Add unit tests for urls

---
+++ todo/tests/test_urls.py
@@ -0,0 +1,24 @@
+from django.test import SimpleTestCase
+from django.urls import reverse, resolve
+from todo.views import task_list, add_task, edit_task, delete_task, toggle_complete
+
+class TestUrls(SimpleTestCase):
+    def test_task_list_url_resolves(self):
+        url = reverse('task_list')
+        self.assertEqual(resolve(url).func, task_list)
+
+    def test_add_task_url_resolves(self):
+        url = reverse('add_task')
+        self.assertEqual(resolve(url).func, add_task)
+
+    def test_edit_task_url_resolves(self):
+        url = reverse('edit_task', args=[1])
+        self.assertEqual(resolve(url).func, edit_task)
+
+    def test_delete_task_url_resolves(self):
+        url = reverse('delete_task', args=[1])
+        self.assertEqual(resolve(url).func, delete_task)
+
+    def test_toggle_complete_url_resolves(self):
+        url = reverse('toggle_complete', args=[1])
+        self.assertEqual(resolve(url).func, toggle_complete)
